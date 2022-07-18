0090808868b90b6d0a8b44b18fae540efa8155df
prashant.prabhakar <prashantprabhakar123@gmail.com>
2022-07-18 23:15:25 +0530

    set difficulty to constant

--- consensus/ethash/consensus.go
+++ consensus/ethash/consensus.go
@@ -308,7 +308,9 @@ func (ethash *Ethash) verifyHeader(chain consensus.ChainHeaderReader, header, pa
 // the difficulty that a new block should have when created at time
 // given the parent block's time and difficulty.
 func (ethash *Ethash) CalcDifficulty(chain consensus.ChainHeaderReader, time uint64, parent *types.Header) *big.Int {
-	return CalcDifficulty(chain.Config(), time, parent)
+	// return CalcDifficulty(chain.Config(), time, parent)
+	// @prashnt: Fixed difficulty to constant to save CPU 
+	return big.NewInt(10)
 }
 
 // CalcDifficulty is the difficulty adjustment algorithm. It returns
